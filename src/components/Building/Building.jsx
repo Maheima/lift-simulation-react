f4a4d394e3485a319e99a567eb96b02daa507456
Mahima Khandelwal <mahi.khandelwal28@gmail.com>
2023-08-24 08:02:29 +0530

    update move lift function and fix styling

--- src/components/Building/Building.jsx
+++ src/components/Building/Building.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Building.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -8,20 +8,52 @@ import {
 
 const Building = () => {
   const [elevatorState, setElevatorState] = useState([]);
-  /**
-   * {
-   * state: busy/free,
-   * currentPos:
-   *
-   * }
-   */
+  const [liftReqQueue, setLiftReqQueue] = useState([]);
+  
+  const [doorStyle, setDoorStyle] = useState({});
+
   const [numberOfFloors, setNumberOfFloors] = useState(1);
+  
+  function removeFloor() {
+    if (numberOfFloors === 1) return;
+    setNumberOfFloors(numberOfFloors - 1);
+  }
+
+  function addFloor() {
+    setNumberOfFloors(numberOfFloors + 1);
+  }
+
+  function addElevator() {
+    const width = window.innerWidth;
+    const numberOfLifts = elevatorState.length + 1;
+    if (width < 769 && numberOfLifts >= (width - 80) / 70) return;
+    if (width >= 769 && numberOfLifts >= (width - 80) / 70) return;
+    setElevatorState((prev) => {
+      return [
+        ...prev,
+        {
+          state: 'free',
+          currentPos: 0,
+          transitionDuration: 0,
+          currentFloor: 0,
+          isDoorOpen: false,
+          dStyle: { width: '50%'},
+        },
+      ];
+    });
+  }
+  function removeElevator() {
+    if (elevatorState.length === 1) return;
+    elevatorState.pop();
+    setElevatorState(() => {
+      return [...elevatorState];
+    });
+  }
 
   const floors = Array.from({ length: numberOfFloors }).map((val, index) => {
-    const calledFloor = numberOfFloors - index;
-
+    const floorNumber = numberOfFloors - index;
     return (
-      <div className="floor_container" id={index}>
+      <div className="floor_container" id={index} key={index}>
         <div className="arrow_container">
           <FontAwesomeIcon
             className="arrow up"
@@ -29,13 +61,13 @@ const Building = () => {
             size="2xl"
             icon={faCircleChevronUp}
           />
-          {calledFloor}
+          {floorNumber}
           <FontAwesomeIcon
             className="arrow down"
             data-testid="arrowDown"
             size="2xl"
             icon={faCircleChevronDown}
-            onClick={() => moveLiftTo(calledFloor)}
+            onClick={() => moveLiftTo(floorNumber)}
           />
         </div>
       </div>
@@ -55,57 +87,73 @@ const Building = () => {
           transition: `all ${duration}s linear`,
         }}
       >
-        <div className="door"></div>
-        <div className="door"></div>
+        <div style={val.dStyle} className="door"></div>
+        <div style={val.dStyle} className="door"></div>
       </div>
     );
   });
 
+  function freeELevatorStates(freeElevatorIndex, duration) {
+    let doorOpeningDuration = duration * 1000;
+    let doorClosingDuration = duration * 1000 + 2500;
+    let liftFreeDuration = duration * 1000 + 5000;
+    
+    setTimeout(() => {
+      let newArray = [...elevatorState];
+      newArray[freeElevatorIndex].dStyle = { width: '0px' , transition: 'all ease-in-out 2.5s'};
+      console.log('Inside 1st setTimeout', freeElevatorIndex);
+      setElevatorState(newArray);
+      console.log('elevatorState 1st timeout', elevatorState)
+
+    }, doorOpeningDuration);
+
+    setTimeout(() => {
+      let newArray = [...elevatorState];
+      newArray[freeElevatorIndex].dStyle = {
+        width: '100%',
+        transition: 'all ease-in-out 5s',
+      };
+      console.log('Inside 2nd setTimeout', freeElevatorIndex);
+      setElevatorState(newArray);
+      console.log('elevatorState 2nd timeout', elevatorState)
+    }, doorClosingDuration);
+    setTimeout(() => {
+      let newArray = [...elevatorState];
+      newArray[freeElevatorIndex].state = 'free';
+      console.log('Inside 3rd setTimeout', freeElevatorIndex);
+      console.log('elevatorState 3rd timeout', elevatorState)
+      if (liftReqQueue.length !== 0) {
+        moveLiftTo(Number(liftReqQueue[0]));
+      }
+      liftReqQueue.shift();
+      setElevatorState(newArray);
+      console.log('liftReqQueue', liftReqQueue)
+    }, liftFreeDuration);
+  }
+
   function moveLiftTo(calledFloor) {
     let distance = (calledFloor - 1) * 110;
     let newState = [...elevatorState];
-    let freeElevators = newState.find((ele) => ele.state === 'free');
-    if (!freeElevators) return;
-    freeElevators.currentPos = distance;
-    freeElevators.state = 'busy';
-    freeElevators.transitionDuration = Math.abs(freeElevators.currentFloor-calledFloor) * 2;
-    freeElevators.currentFloor = calledFloor;
-    console.log('first', freeElevators);
+    let freeElevator = newState.find((ele) => ele.state === 'free');
+    let freeElevatorIndex = newState.findIndex((ele) => ele.state === 'free');
+    if (!freeElevator) {
+      setLiftReqQueue([...liftReqQueue, calledFloor])
+      console.log('liftReqQueue', liftReqQueue);
+      return;
+    }
+    freeElevator.currentPos = distance;
+    freeElevator.state = 'busy';
+  
+    freeElevator.transitionDuration =
+      Math.abs(Number(freeElevator.currentFloor) - Number(calledFloor)) * 2;
+
+    freeElevator.currentFloor = calledFloor;
+    freeELevatorStates(
+      freeElevatorIndex,
+      freeElevator.transitionDuration
+    );
     setElevatorState([...newState]);
-
-    setTimeout(() => {
-      freeElevators.state = 'free';
-    }, 1000 * 2 + 5000);
-  }
-
-  function removeFloor() {
-    if (numberOfFloors === 1) return;
-    setNumberOfFloors(numberOfFloors - 1);
-  }
-
-  function addFloor() {
-    setNumberOfFloors(numberOfFloors + 1);
-  }
-
-  function addElevator() {
-    setElevatorState((prev) => {
-      return [
-        ...prev,
-        {
-          state: 'free',
-          currentPos: 0,
-          transitionDuration: 0,
-          currentFloor: 0
-        },
-      ];
-    });
-  }
-  function removeElevator() {
-    if (elevatorState.length === 1) return;
-    elevatorState.pop();
-    setElevatorState(() => {
-      return [...elevatorState];
-    });
+    console.log('elevatorState moveLiftTo', elevatorState)
   }
 
   return (
